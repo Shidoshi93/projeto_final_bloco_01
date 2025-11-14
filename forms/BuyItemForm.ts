37707023aa9c0d70e8ad998b4844b7dc96d29fc2
Daniel Ribeiro <dann.ribeiroo@gmail.com>
2025-11-14 11:00:12 -0300

    refactor: convert BuyItemForm to class

--- forms/BuyItemForm.ts
+++ forms/BuyItemForm.ts
@@ -8,38 +8,40 @@ interface BuyItemDetails {
     typeOfDelivery: string | undefined;
 }
 
-const typeOfDeliveryOptions: string[] = [
-    "Standard Shipping",
-    "Express Shipping",
-    "In-Store Pickup"
-];
+export class BuyItemForm {
+    private typeOfDeliveryOptions: string[] = [
+        "Standard Shipping",
+        "Express Shipping",
+        "In-Store Pickup"
+    ];
 
-export function buyItem(): void {
-    console.log("Buy Item Form");
+    public buyItem(): void {
+        console.log("Buy Item Form");
 
-    const itemId: string = question("Enter the Item ID to purchase: ");
-    const quantity: number = parseInt(question("Enter quantity to purchase: "), 10);
-    const paymentMethod: string = question("Enter payment method (e.g., credit card, PayPal): ");
-    const typeOfDeliveryIndex: number = keyInSelect(typeOfDeliveryOptions, "Select type of delivery:") + 1;
-    
-    if (typeOfDeliveryIndex === -1) {
-        console.log("No delivery option selected. Purchase cancelled.");
-        return;
+        const itemId: string = question("Enter the Item ID to purchase: ");
+        const quantity: number = parseInt(question("Enter quantity to purchase: "), 10);
+        const paymentMethod: string = question("Enter payment method (e.g., credit card, PayPal): ");
+        const typeOfDeliveryIndex: number = keyInSelect(this.typeOfDeliveryOptions, "Select type of delivery:");
+        
+        if (typeOfDeliveryIndex === -1) {
+            console.log("No delivery option selected. Purchase cancelled.");
+            return;
+        }
+
+        const typeOfDelivery: string | undefined = this.typeOfDeliveryOptions[typeOfDeliveryIndex];
+
+        const shippingAddress: string = question("Enter shipping address: ");
+
+        const purchaseDetails: BuyItemDetails = {
+            itemId,
+            quantity,
+            paymentMethod,
+            shippingAddress,
+            typeOfDelivery
+        };
+
+        // call the controller function to handle item purchase here
+
+        console.log(`Successfully purchased ${quantity} of item ID: ${itemId}`);
     }
-
-    const typeOfDelivery: string | undefined= typeOfDeliveryOptions[typeOfDeliveryIndex];
-
-    const shippingAddress: string = question("Enter shipping address: ");
-
-    const purchaseDetails: BuyItemDetails = {
-        itemId,
-        quantity,
-        paymentMethod,
-        shippingAddress,
-        typeOfDelivery
-    };
-
-    // call the controller function to handle item purchase here
-
-    console.log(`Successfully purchased ${quantity} of item ID: ${itemId}`);
 }
