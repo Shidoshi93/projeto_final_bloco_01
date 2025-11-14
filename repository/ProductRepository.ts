2b37582a749682381d511e1a51dbf897b8eac40d
Daniel Ribeiro <dann.ribeiroo@gmail.com>
2025-11-14 14:26:31 -0300

    refactor: remove static declaration

--- repository/ProductRepository.ts
+++ repository/ProductRepository.ts
@@ -11,36 +11,34 @@ type Product = {
 };
 
 export class ProductRepository implements ProductInterface {
-    private static products: Product[] = mockProducts;
-
-    private static nextId: number = 15;
+    private products: Product[] = [...mockProducts];
+    private nextId: number = 16;
 
     public addProduct(product: Product): void {
-        
-        const newProduct = { ...product, id: ProductRepository.nextId++ };
-        ProductRepository.products.push(newProduct);
+        const newProduct = { ...product, id: this.nextId++ };
+        this.products.push(newProduct);
     }
 
     public getProductById(id: number): Product | null {
-        const product = ProductRepository.products.find(p => p.id === id);
+        const product = this.products.find(p => p.id === id);
         return product || null;
     }
 
     public updateProduct(id: number, updatedProduct: Product): void {
-        const index = ProductRepository.products.findIndex(p => p.id === id);
+        const index = this.products.findIndex(p => p.id === id);
         if (index !== -1) {
-            ProductRepository.products[index] = { ...updatedProduct, id };
+            this.products[index] = { ...updatedProduct, id };
         }
     }
 
     public deleteProduct(id: number): void {
-        const index = ProductRepository.products.findIndex(p => p.id === id);
+        const index = this.products.findIndex(p => p.id === id);
         if (index !== -1) {
-            ProductRepository.products.splice(index, 1);
+            this.products.splice(index, 1);
         }
     }
 
     public listAllProducts(): Product[] {
-        return [...ProductRepository.products];
+        return [...this.products];
     }
 }
