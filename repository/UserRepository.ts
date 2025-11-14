61196e3e4448ad31b5ef444d3637a103bb92239e
Daniel Ribeiro <dann.ribeiroo@gmail.com>
2025-11-14 15:23:59 -0300

    feat: creates delete user and addAddresUser repository

--- repository/UserRepository.ts
+++ repository/UserRepository.ts
@@ -1,4 +1,5 @@
 import { UserInterface } from "../interfaces/UserInterface";
+import { Endereco } from "../model/Endereco";
 import User from "../types/UserTypes";
 
 export class UserRepository implements UserInterface {
@@ -24,6 +25,24 @@ export class UserRepository implements UserInterface {
             return false;
         }
         this.users[userIndex] = { username, password, email };
+        return true;
+    }
+
+    public deleteUser(username: string): boolean {
+        const userIndex = this.users.findIndex(u => u.username === username);
+        if (userIndex === -1) {
+            return false;
+        }
+        this.users.splice(userIndex, 1);
+        return true;
+    }
+
+    addressUser(username: string, endereco: Endereco): boolean {
+        const user = this.users.find(u => u.username === username);
+        if (!user) {
+            return false;
+        }
+        user.endereco = endereco;
         return true;
     }
 }
--- types/UserTypes.ts
+++ types/UserTypes.ts
@@ -1,7 +1,10 @@
+import { Endereco } from "../model/Endereco";
+
 type User = {
     username: string;
     password: string;
     email: string;
+    endereco?: Endereco;
 };
 
 export default User;
